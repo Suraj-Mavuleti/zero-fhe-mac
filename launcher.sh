62ee8e446bef5a65da5aca553a0214f7e0d7aa44
Suraj Mavuleti <surajmavuleti@gmail.com>
2026-09-06 20:21:50 +0530

    Feat(v8): Visual Overhaul using CustomTkinter Modern UI Framework

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# AUTO-UPDATER
 cd /home/suraj/.gemini/antigravity/scratch/god_tier_suite/zero-fhe-mac
 git pull origin main --quiet
-python3 zero_fhe_gui.py
+/home/suraj/.gemini/antigravity/scratch/v8_env/bin/python3 zero_fhe_gui.py
